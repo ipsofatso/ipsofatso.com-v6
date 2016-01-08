feed4d7cf73c28a2ddfff623dd0db964b1db759f
ben <bengarcia@ipsoimac-10.attlocal.net>
2016-01-08 00:33:53 -0800

    added analytics, and source code link.

--- js/components/about.js
+++ js/components/about.js
@@ -17,7 +17,7 @@ var About = React.createClass({
             <div className={ className } style={ style }>
                 <p>Born and raised in southern California, and based in Pasadena. Ipso/Fatso has been spreading its flavor of design to the masses since 2004.</p>
                 <p>It would be nice to hear about your project and what I can do to help, send me an email and I will be in contact.</p>
-                <p>This site was created using React, SASS, Lodash, and build with Gulp. The animation is all css, no jQuery.</p>
+                <p>This site was created using React, SASS, Lodash, and build with Gulp. The animation is all css, no jQuery. <a href="https://github.com/ipsofatso/ipsofatso.com-v6">Source code</a></p>
             </div>
         );
     }
